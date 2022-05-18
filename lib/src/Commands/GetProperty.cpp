52f7a2a5a48576803c18c0ea9cca37fe6e56dabf
ctmbl <mabileau.clement@gmail.com>
2022-05-18 11:14:19 +0200

    Add better logs for getStringProperty

--- lib/src/Commands/GetProperty.cpp
+++ lib/src/Commands/GetProperty.cpp
@@ -27,6 +27,7 @@ void GetProperty::execute(CommandEnvironment& env)
 {
     auto obj = env.scene().objectAtPath(m_path);
 
+    // Checks whether the targeted object can be found
     if (!obj){
         m_promise.set_value("");
         env.state().reportError("GetProperty: Item not found: " + m_path.string());
@@ -36,22 +37,45 @@ void GetProperty::execute(CommandEnvironment& env)
     QQmlContext* const context = qmlContext(obj);
     QVariant value;
 
+    // Checks if the property is a context property 
+    // (context properties can overwrite properties so checking it first gives, intentionally, priority to context)
     if (context) {
         value = context->contextProperty(m_propertyName.c_str());
+
+        // If the QVariant property is valid (meaning property has be found) it doesn't check "classic" properties then (return anyway)
         if (value.isValid()){
-            m_promise.set_value(value.toString().toStdString());
+
+            // Checks if the property can actually be converted to a string
+            auto valueString = value.toString().toStdString();
+            if (!valueString.empty()){ 
+                m_promise.set_value(valueString);
+                return;
+            }
+            m_promise.set_value("");
+            env.state().reportError("GetProperty: Property '" + m_propertyName + "' in Object at path: " + m_path.string() + " can't be converted to string.");
             return;
         }
     }
+
+
     value = obj->property(m_propertyName.c_str());
 
     if (value.isValid()){
-        m_promise.set_value(value.toString().toStdString());
-        return;
-    } else {
+
+        // Checks if the property can actually be converted to a string
+        auto valueString = value.toString().toStdString();
+        if (!valueString.empty()){
+            m_promise.set_value(valueString);
+            return;
+        }
         m_promise.set_value("");
-        env.state().reportError("GetProperty: Property not found: '" + m_propertyName + "' in Object at path: " + m_path.string());
+        env.state().reportError("GetProperty: Property '" + m_propertyName + "' in Object at path: " + m_path.string() + " can't be converted to string.");
+        return;
     }
+
+    // No property QVariant property (context and classic) is valid, meaning the property doesn't exists for this Object
+    m_promise.set_value("");
+    env.state().reportError("GetProperty: Property not found: '" + m_propertyName + "' in Object at path: " + m_path.string());
 }
 
 } // namespace cmd
